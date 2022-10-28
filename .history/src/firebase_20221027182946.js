4bd154feee91f9d371150362560b418a70701b18
Mardoqueu <mdq767@gmail.com>
2022-10-28 09:08:23 -0300

    Install firebase and react-toastify and sign up user

---
+++ .history/src/firebase_20221027182946.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDwdtXjMQLSYx17LyYzQz_wcPjc5jx1L5g",
+  authDomain: "realtor-clone-react-57d40.firebaseapp.com",
+  projectId: "realtor-clone-react-57d40",
+  storageBucket: "realtor-clone-react-57d40.appspot.com",
+  messagingSenderId: "760818294843",
+  appId: "1:760818294843:web:ff5cf50daaee7d16547c1e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ .history/src/firebase_20221028090509.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain:  process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket:  process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: "1:760818294843:web:ff5cf50daaee7d16547c1e"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export const db = getFirestore();
