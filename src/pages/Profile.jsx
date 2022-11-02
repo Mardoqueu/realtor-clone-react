3d61cf494b0793fca5414ccd4a003e02f81cffea
Mardoqueu Sousa <mdq767@gmail.com>
2022-11-02 14:19:48 -0300

    add the edit functionality to the profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,19 @@
-import { getAuth } from 'firebase/auth';
-import React from 'react';
+import { getAuth, updateProfile } from 'firebase/auth';
+import { doc, updateDoc } from "firebase/firestore";
 import { useState } from 'react';
 import { useNavigate } from 'react-router';
+import { toast } from 'react-toastify';
+import { db } from "../firebase";
+
 
 const Profile = () => {
-    /* So firth we got the information from the auth and put it inside formData, but we can not get it directly because we would get an error, so it needs to wait until the information is coming from the auth, that's why I have to added the middleware */
+    /* So first we got the information from the auth and put it inside formData, but we can not get it directly because we would get an error, so it needs to wait until the information is coming from the auth, that's why I have to added the middleware */
     const auth = getAuth();
     const navigate = useNavigate();
+    const [changeDetail, setChangeDetail] = useState(false);
     const [formData, setFormData] = useState({
-        name: "Mardoqueu",
-        email: "mardoqueu.sousa@gmail.com"
+        name: auth.currentUser.displayName,
+        email: auth.currentUser.email,
     });
 /*     structure and name and email from formData
  */    
@@ -19,6 +23,33 @@ const Profile = () => {
         auth.signOut();
         navigate("/");
     }
+    function onChange(e) {
+        setFormData((prevState) => ({
+          ...prevState,
+          [e.target.id]: e.target.value,
+        }));
+      }
+      async function onSubmit() {
+        try {
+          if (auth.currentUser.displayName !== name) {
+            //update display name in firebase auth
+            await updateProfile(auth.currentUser, {
+              displayName: name,
+            });
+    
+            // update name in the firestore
+    
+            const docRef = doc(db, "users", auth.currentUser.uid);
+            await updateDoc(docRef, {
+              name,
+            });
+          }
+          toast.success("Profile details updated");
+        } catch (error) {
+          toast.error("Could not update the profile details");
+        }
+      }
+
     return (
         <>
             <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
@@ -29,14 +60,31 @@ const Profile = () => {
                     <form>
                         {/* Name input*/}
 
-                        <input type="text" id="name" value={name} disabled className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out'></input>
+                        <input 
+                        type="text" 
+                        id="name" 
+                        value={name} 
+                        disabled={!changeDetail}
+                        onChange={onChange}
+                        className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${
+                            changeDetail && "bg-red-200 focus:bg-red-200"
+                        }`}></input>
 
                         {/* Email input*/}
                         <input type="email" id="email" value={email} disabled className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out'></input>
 
                         <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6'>
                             <p className='flex items-center '>Do you want to change your name?
-                            <span className='text-red-600 hover:text-red-700 ease-in-out duration-200 ml-1 cursor-pointer'>Edit</span>
+                            <span
+                                onClick={() => {
+                                    changeDetail && onSubmit();
+                                    setChangeDetail((prevState) => !prevState);
+                                }}
+                                className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer"
+                                >
+                                {changeDetail ? "Apply change" : "Edit"}
+                             
+                             </span>
                             </p>
                             <p onClick={onLogout} className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer'>Sign out</p>
                         </div>
