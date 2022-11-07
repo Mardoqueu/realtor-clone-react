1bd7fb59b09435d8f5ef04532a215bbdf6fcaac2
Mardoqueu Sousa <mdq767@gmail.com>
2022-11-07 16:34:54 -0300

    listingItem compoment complete

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,4 +1,41 @@
-/* got the information like a listing and ID, and show that inside an empty div */
+import Moment from "react-moment";
+import { Link } from "react-router-dom";
+import {MdLocationOn} from 'react-icons/md'
+
 export default function ListingItem({ listing, id }) {
-    return <div>{listing.name}</div>;
+    /* Listing items we have created the listing */
+    return <li className="relative bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition duration-150 m-[10px]">
+        <Link className="contents" to={`/category/${listing.type}/${id.type}`}>
+        <img className="h-[170px] w-full object-cover hover:scale-105 transition-scale duration-200 ease-in-out" 
+        loading="lazy"
+        src={listing.imgUrls[0]}></img>
+        {/* Adding moment to get the correct time */}
+        <Moment className="absolute top-2 left-2 bg-[#3377cc] text-white uppercase text-xs font-semibold rounded-md px-2 py-1 shadow-lg" fromNow>{listing.timestamp?.toDate()}</Moment>
+            <div className="w-full p-[10px]">
+                <div className="flex items-center space-x-1">
+                    <MdLocationOn className="h-4 w-4 text-green-600"/>
+                    <p className="font-semibold text-sm mb-[2px] text-green-600 truncate">{listing.address}</p>
+                </div>
+            <p className="font-semibold m-0 text-xl truncate">{listing.name}</p>
+            <p className="text-[#457b9d] mt-2 font-semibold">${listing.offer 
+            ? listing.discountedPrice
+                .toString()            
+                .replace(/\B(?=(\d{3})+(!\d))/g, ",") 
+            : listing.regularPrice
+                .toString()            
+                .replace(/\B(?=(\d{3})+(!\d))/g, ",")}
+            {listing.type === "rent" && " / month"}   
+                </p>
+                <div className="flex items-center mt-[10px] space-x-3">
+                    <div className="flex items-center space-x-1">
+                        <p className="font-bold text-xs">{listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}</p>
+                    </div>
+                    <div className="flex items-center space-x-1">
+                        <p className="font-bold text-xs">
+                        {listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : "1 Bath"}</p>
+                    </div>
+                </div>
+            </div>
+        </Link>
+    </li>;
   }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -124,8 +124,8 @@ const Profile = () => {
             <div className='max-w-6xl px-3 mt-6 mx-auto'>
             {!loading && listings.length > 0 && (
           <>
-            <h2 className="text-2xl text-center font-semibold">My Listings</h2>
-            <ul>
+            <h2 className="text-2xl text-center font-semibold mb-6 mt-6">My Listings</h2>
+            <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl-grid-cols-5 mt-6 mb-6'>
               {listings.map((listing) => (
                 <ListingItem
                   key={listing.id}
