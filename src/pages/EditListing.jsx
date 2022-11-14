a3350b9751c131891fa903e23aa2b84d1b7fb1bf
Mardoqueu Sousa <mdq767@gmail.com>
2022-11-14 18:24:44 -0300

    complete home page by adding recent offers, places for rent and place for sale

--- src/pages/EditListing.jsx
+++ src/pages/EditListing.jsx
@@ -366,7 +366,7 @@ const EditListing = () => {
                 id='description' 
                 value={description} 
                 onChange={onChange} 
-                placeholder='Address' 
+                placeholder='Description' 
                 required 
                 className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mg-6'></textarea>
 
